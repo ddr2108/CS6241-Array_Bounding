b151900357cdcce1cd3a258351848f0de6527f45
ddr2108 <deepdattaroy8888@gmail.com>
2014-04-14 13:25:47 -0400

    Test file

--- Work/Test/hello.cpp
+++ Work/Test/hello.cpp
@@ -3,56 +3,99 @@
 
 int main()
 {
-int d,z;
-z=7;
-if (z>5){
-	d=7;
-}else{
-	d=9;
-}
+	int d,z, k, l,j, f,g;
+	int m[5];	
 
-z= z + 15;
+	d = 15;
+	z = 15;
+	//m[4] = 10 + z;
+	//m[4] = 5 + z;
+	l = 10 + m[4];
+	d = 10 + m[4];
+	m[4] = 10 + z;
+	d = 18 + z;
+	d = 15;
+	//d = 115 + l;
+	//z = l + 115;
+	//z = 115 + l;
+	if (z>d){
+k=5;
+//d = 12;
+		//d =  5;
+		j= k + 5;
+	}else{
+k=10;
+		//d= l + 15;
+		//z= l + 15;
+	}
+ //l =5;
+ //z =d;
+	l = k + 5;
+	f = d+5;
+
+	if (d>z){
+		j=25;
+	}else{
+		k=9;
+	}
+ 
+	f = 18 + z;
+f=5;
+	g = d + 5;
+	//f = d+8;
+l = 5;
+//f = 8;
+d = 8;
+if (z>5){
+		j=25;
+	}else{
+		//k=9;
+	}
+ 
+	k = 18 + z;
+	l = d + 8;
+	f = d+5;
+
+
+/*
+	if (d>5){
+		z=5;
+	}else{
+		z=19;
+	}
+	l = k+15;
+	k = k+15;
+
+
+
+
+	if (l>5){
+		l=7;
+	}else{
+		l=9;
+	}
+	
 	d=d-5;
-if (z>5){
-	z=7;
-}else{
-	z=9;
-/*int test(){
-	int size = 25;
-	int a[size];
-	bool branchIf = false;
+	k = z + 5;
 
-	int x,y;
-	int z;
+	if (k>5){
+		l=8;
+	}else{
+		l=10;}
 
-	x = 2;
-	y = 2*x+1;
-	z = 1*x + 1;
-	int b[y];
-	//a[y] = 3;
-	//y = z;
-	printf("Before");
-	b[z] = 10;
-	printf("After");
-	if(branchIf)printf("Branch");
-	printf("Before");
-	b[4] = 10;
-	printf("After");
-	if(branchIf)printf("Branch");
-	printf("Before");
-	a[5] = 10;
-	printf("After");
-	if(branchIf)printf("Branch");
-	printf("Before");
-	a[28] = 10;
-	printf("After");
-	if(branchIf)printf("Branch");
-	x = a[30];	
-	return 1;
-}*/
-
-	z = z-5;
+	if (z>8){
+		z=7;
+	}else{
+		z=9;
+	}
 
 
+	if (k>8){
+		l=9;
+	}else{
+		l=11;
+	}
+
+	z= z + 15;*/
 }
 
